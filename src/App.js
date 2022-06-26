0d46b6683fb5323f8f89719978d399bf0aaaa6bb
hding2196 <hding2196@gmail.com>
2022-06-26 21:29:22 +0700

    Create AppDemo
    Tạo file Appdemo để test github

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import { AppDemo } from './components/demo/AppDemo';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <AppDemo/>
     </div>
   );
 }
---
+++ src/components/demo/AppDemo.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const AppDemo = () => {
+  return (
+    <div>AppDemoaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</div>
+  )
+}
