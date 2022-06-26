60513ffc954a8c2f3d22b9c602ffdf8c0e76ce4a
TuanNguyen <82533800+tuannguyen-studio@users.noreply.github.com>
2022-06-26 21:47:44 +0700

    111
    hello

--- src/components/demo/AppDemo.jsx
+++ src/components/demo/AppDemo.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 
 export const AppDemo = () => {
   return (
+<<<<<<< Updated upstream
     <div>Hello World</div>
+=======
+<div>Hello, I am Tuan Nguyen Trung</div>
+>>>>>>> Stashed changes
   )
 }
